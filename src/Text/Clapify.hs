eec0ac6ff4172e0af8ed2243eb0a70be5bbf0d1f
Vanessa McHale <vanessa.mchale@iohk.io>
2018-12-12 11:53:58 -0600

    Fix base bounds + add printClap

--- src/Text/Clapify.hs
+++ src/Text/Clapify.hs
@@ -1,8 +1,12 @@
 module Text.Clapify
     ( clapify
+    , printClap
     ) where
 
 import           Data.List (intercalate)
 
 clapify :: String -> String
 clapify = (++ " 👏") . intercalate " 👏 " . words
+
+printClap :: String -> IO ()
+printClap = putStrLn . clapify
