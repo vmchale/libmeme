9f318264126ec15e3e70b11ecf543bd38e1f38ae
Vanessa McHale <vanessa.mchale@iohk.io>
2019-08-05 17:42:44 -0500

    Add some more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,8 +10,17 @@ isIdempotent f = \str -> f (f str) == f str
 idempotentProp :: (String -> String) -> SpecWith ()
 idempotentProp = parallel . prop "should be idempotent" . isIdempotent
 
+conjugate :: (String -> String) -> (String -> Bool)
+conjugate f = \str -> reverse (f (reverse str)) == f str
+
+conjugateProp :: (String -> String) -> SpecWith ()
+conjugateProp = parallel . prop "reverse . f . reverse ≡ f" . conjugate
+
 main :: IO ()
-main = hspec $
-    describe "spongebob" $
+main = hspec $ do
+    describe "idempotency" $
         traverse_ idempotentProp
             [spongebobCase, spongebobZygo, toFraktur, toScript, toBlackboardBold]
+    describe "conjugation" $
+        traverse_ conjugateProp
+            [toFraktur, toScript, toBlackboardBold]
