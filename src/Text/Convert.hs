d3620cb2711c3ddeb4970cf12873479562459734
Vanessa McHale <vamchale@gmail.com>
2019-10-14 08:36:29 -0500

    format?

--- src/Text/Convert.hs
+++ src/Text/Convert.hs
@@ -7,8 +7,8 @@ module Text.Convert ( toFraktur
                     ) where
 
 import           Text.Convert.BlackboardBold
+import           Text.Convert.Bubble
 import           Text.Convert.Fraktur
 import           Text.Convert.Monospace
 import           Text.Convert.Script
 import           Text.Convert.Wide
-import           Text.Convert.Bubble
